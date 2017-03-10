de54f7b9f15bb876f7b84a86fa9e0a0789d67f93
Franky Martin <fmartin@neutral.io>
2017-03-10 10:00:52 -0500

    NSECURITY-35, providing eval's globals namespace with allowed functionality

--- nio/properties/util/safe_eval.py
+++ nio/properties/util/safe_eval.py
@@ -8,25 +8,34 @@ class SafeEval(object):
     # this black list is presented to provide context, not that all these
     # ops are dangerous just that some of them don't seem useful to have
     #
-    #     classmethod, compile, dict, dir, eval, exec, filter, frozenset,
-    #     globals, help, input, iter, locals, memoryview, open, property,
-    #     range, staticmethod, super, vars, __import__
+    #     classmethod, compile, dir, eval, exec, frozenset,
+    #     globals, help, input, locals, memoryview, open, property,
+    #     staticmethod, super, vars, __import__
     #
 
     # define white-listed builtins
     _white_listed_builtins = [
-        abs, all, any, ascii, bin, bool, bytearray, bytes, callable, chr,
-        complex, delattr, divmod, enumerate, float, format, getattr,
-        hasattr, hash, hex, id, int, isinstance, issubclass, len, list,
-        map, max, min, next, object, oct, ord, pow, print, repr, reversed,
-        round, set, setattr, slice, sorted, str, sum, tuple, type, zip
+        abs, all, any, ascii, bin, bool, bytearray, bytes,
+        callable, chr, complex, delattr, dict, divmod,
+        enumerate, filter, float, format, getattr,
+        hasattr, hash, hex, id, iter, int, isinstance, issubclass,
+        len, list, map, max, min, next, object, oct, ord,
+        pow, print, range, repr, reversed, round,
+        set, setattr, slice, sorted, str, sum, tuple, type, zip
     ]
 
     # defines default modules to allow within expressions
     _modules = ["datetime", "json", "math", "random", "re"]
-    # keeps a mapping at instance level holding white-listed modules
+    # keeps eval args at instance level holding white-listed modules
     # and functions
-    _mapping = None
+
+    # a globals namespace is needed to define pretty much the same
+    # functionality that is allowed within the locals namespace, it
+    # has been noted that depending on the expression, python's looks
+    # for the name in the globals namespace rather than the local
+    # namespace (expressions including 'for' constructs in them.)
+    _globals_namespace = None
+    _locals_namespace = None
 
     @classmethod
     def set_modules(cls, modules):
@@ -36,8 +45,9 @@ class SafeEval(object):
             modules (list): list of modules to allow
         """
         cls._modules = modules
-        # mapping needs to be rebuilt
-        cls._mapping = None
+        # eval args need to be rebuilt
+        cls._globals_namespace = None
+        cls._locals_namespace = None
 
     @classmethod
     def eval(cls, expression, signal):
@@ -46,46 +56,60 @@ class SafeEval(object):
         Args:
             expression (str): expression to evaluate
             signal (Signal): signal to draw attributes from if needed
+
+        Raises:
+            TypeError: when accessing not-allowed functionality or there
+                is a problem with the expression
+            SyntaxError: when the python construct contains a syntax error
         """
-        _locals_mapping = cls._get_locals_mapping()
+        cls._ensure_eval_args()
+
         # add signal so that it can be referenced
-        _locals_mapping["signal"] = signal
+        cls._globals_namespace["signal"] = signal
+        cls._locals_namespace["signal"] = signal
 
         # evaluate
-        result = eval(expression, {"__builtins__": None}, cls._mapping)
+        result = eval(expression, cls._globals_namespace, cls._locals_namespace)
 
         # no need to keep signal entry
-        del _locals_mapping["signal"]
+        del cls._globals_namespace["signal"]
+        del cls._locals_namespace["signal"]
 
         return result
 
     @classmethod
-    def _get_locals_mapping(cls):
-        """ Returns locals mapping to use for 'eval'
-
-        Manages whether mappings are available, if not, it makes
-        sure to build and save them.
-        """
-        if cls._mapping is None:
-            cls._mapping = cls._build_locals_mapping()
-
-        return cls._mapping
-
-    @classmethod
-    def _build_locals_mapping(cls):
-        """ Builds a locals mapping to use for eval
+    def _ensure_eval_args(cls):
+        """ Ensures args to use for eval are up to date
 
         locals mapping are built using the modules allowed
         through 'set_modules' and using the white-listed functions
         from __builtins__
         """
-        locals_mapping = {}
+        if cls._globals_namespace is None or cls._locals_namespace is None:
+            cls._globals_namespace, cls._locals_namespace = \
+                cls._build_eval_args()
+
+    @classmethod
+    def _build_eval_args(cls):
+        """ Builds eval arguments
+
+        globals namespace is started by 'invalidating' all __builtins__, and
+        then adding allowed functionality
+
+        Returns:
+            tuple containing globals and locals namespaces
+        """
+        globals_namespace = {"__builtins__": None}
+        locals_namespace = {}
 
         # import allowed modules
         for module in cls._modules:
-            locals_mapping[module] = __import__(module)
+            locals_namespace[module] = __import__(module)
+            globals_namespace[module] = locals_namespace[module]
 
         # add white-listed builtins
         for builtin in SafeEval._white_listed_builtins:
-            locals_mapping[builtin.__name__] = builtin
-        return locals_mapping
+            locals_namespace[builtin.__name__] = builtin
+            globals_namespace[builtin.__name__] = builtin
+
+        return globals_namespace, locals_namespace
--- nio/properties/util/tests/test_safe_eval.py
+++ nio/properties/util/tests/test_safe_eval.py
@@ -51,13 +51,14 @@ class TestSafeEval(NIOTestCaseNoModules):
         """ Assert that locals_mapping is only built when needed
         """
         expression = "{{ 'hello' }}"
-        with patch.object(SafeEval, "_build_locals_mapping") as patched_build:
+        with patch.object(SafeEval, "_build_eval_args") as patched_build:
+            patched_build.return_value = {}, {}
             self.assertEqual(patched_build.call_count, 0)
             evaluator = Evaluator(expression)
             evaluator.evaluate(None)
             self.assertEqual(patched_build.call_count, 1)
 
-            # evaluate again and assert that '_build_locals_mapping'
+            # evaluate again and assert that '_build_eval_args'
             # was not called this time
             evaluator.evaluate(None)
             self.assertEqual(patched_build.call_count, 1)
@@ -74,3 +75,186 @@ class TestSafeEval(NIOTestCaseNoModules):
         evaluator = Evaluator(expression)
         with self.assertRaises(TypeError):
             evaluator.evaluate(None)
+
+        expression = "{{for something}}"
+        evaluator = Evaluator(expression)
+        with self.assertRaises(SyntaxError):
+            evaluator.evaluate(None)
+
+    def test_expressions(self):
+        """ Asserts expressions encountered in production environments
+        """
+
+        expression = "{{float(1.2)}}"
+        evaluator = Evaluator(expression)
+        result = evaluator.evaluate(None)
+        self.assertEqual(result, 1.2)
+
+        data = {"type": "D",
+                "name": "name",
+                "zone": "zone",
+                "group": "group",
+                "units": "units"}
+        signal = Signal(data)
+        expression = \
+            "{{[{" \
+            "'terminal': x+1, " \
+            "'type': y[0], " \
+            "'name': y[1] if y[1] != '_' else None, " \
+            "'zone': y[2] if y[2] != '_' else None, " \
+            "'group': y[3] if y[3] != '_' else None, " \
+            "'units': y[4] if y[4] != '_' else None, " \
+            "'limits': {'lolo': None, 'lo': None, 'hi': None, 'hihi': None} } "\
+            "for x,y in enumerate(zip($type, $name, $zone, $group, $units)) " \
+            "if y[0] == 'D'] }}"
+        evaluator = Evaluator(expression)
+        result = evaluator.evaluate(signal)
+        self.assertTrue(result[0]['group'], 'group')
+        self.assertTrue(isinstance(result[0]['limits'], dict))
+
+        data = {"type": "A",
+                "name": "name",
+                "zone": "zone",
+                "group": "group",
+                "min": "1",
+                "max": "2",
+                "units": "D",
+                "lolo": "1.1",
+                "lo": "3",
+                "hi": "4",
+                "hihi": "5",
+                }
+        signal = Signal(data)
+        expression = \
+            "{{ [{" \
+            "'terminal': x+1, " \
+            "'type': y[0], " \
+            "'name': y[1] if y[1] != '_' else None, " \
+            "'min': float(y[2]) if y[2].split('.')[0].isdigit() else 0, " \
+            "'max': float(y[3]) if y[3].split('.')[0].isdigit() else 0, " \
+            "'zone' : y[4] if y[4] != '_' else None,  " \
+            "'group': y[5] if y[5] != '_' else None, " \
+            "'units': y[6] if y[6] != '_' else None, " \
+            "'limits': {'lolo': float(y[7]) if y[7] != '_' else None, " \
+            "'lo': float(y[8]) if y[8] != '_' else None, " \
+            "'hi': float(y[9]) if y[9] != '_' else None, " \
+            "'hihi': float(y[10]) if y[10] != '_' else None} } " \
+            "for x,y in enumerate(zip($type, $name, $min, $max, $zone, $group,"\
+            "$units, $lolo, $lo, $hi, $hihi)) if y[0] == 'A'] }}"
+        evaluator = Evaluator(expression)
+        result = evaluator.evaluate(signal)
+        self.assertTrue(result[0]['zone'], 'zone')
+        self.assertTrue(result[0]['limits']['lolo'], 1.1)
+        self.assertTrue(isinstance(result[0]['limits'], dict))
+
+        expression = \
+            "{{ [{" \
+            "'terminal': x+1, " \
+            "'type': y[0], " \
+            "'name': y[1] if y[1] != '_' else None, " \
+            "'min': float(y[2]) if y[2].split('.')[0].isdigit() else 0, " \
+            "'max': float(y[3]) if y[3].split('.')[0].isdigit() else 0, " \
+            "'zone' : y[4] if y[4] != '_' else None,  " \
+            "'group': y[5] if y[5] != '_' else None, " \
+            "'units': y[6] if y[6] != '_' else None, " \
+            "'limits': float(y[7]) if y[7] != '_' else None, " \
+            " } " \
+            "for x,y in enumerate(zip($type, $name, $min, $max, $zone, $group,"\
+            "$units, $lolo, $lo, $hi, $hihi)) if y[0] == 'A'] }}"
+        evaluator = Evaluator(expression)
+        result = evaluator.evaluate(signal)
+        self.assertEqual(result[0]['min'], 1)
+        self.assertEqual(result[0]['max'], 2)
+
+        data = {"type": "T",
+                "name": "name",
+                "zone": "zone",
+                "group": "group",
+                "min": "1",
+                "max": "2",
+                "units": "units",
+                "lolo": "1",
+                "lo": "2",
+                "hi": "3",
+                "hihi": "4",
+                }
+        signal = Signal(data)
+        expression = \
+            "{{[{'terminal': x + 1, " \
+            "'type': y[0], " \
+            "'name': y[1] if y[1] != '_' else None," \
+            "'zone': y[2] if y[2] != '_' else None," \
+            "'group': y[3] if y[3] != '_' else None," \
+            "'units': y[4] if y[4] != '_' else None," \
+            "'limits': {" \
+                "'lolo': float(y[5]) if y[5] != '_' else None," \
+                "'lo': float(y[6]) if y[6] != '_' else None, " \
+                "'hi': float(y[7]) if y[7] != '_' else None, "\
+                "'hihi': float(y[8]) if y[8] != '_' else None } } "\
+            "for x, y in enumerate(zip($type, $name, $zone, $group, $units, " \
+            "$lolo, $lo, $hi, $hihi)) if y[0] == 'T']}}"
+
+        evaluator = Evaluator(expression)
+        result = evaluator.evaluate(signal)
+        self.assertEqual(result[0]['limits']['lolo'], 1)
+
+        signal = Signal({'cond': True})
+
+        # assert reference to a signal attr
+        expression = "{{$.cond if $.cond else None}}"
+        evaluator = Evaluator(expression)
+        result = evaluator.evaluate(signal)
+        self.assertTrue(result)
+
+        # assert reference to a signal attr within dict, it turns out that
+        # the signal reference that is used is the one defined in globals
+        expression = "{{[{'entry': math.fabs(i) if $.cond else None} " \
+                     "for i in [1]]}}"
+        evaluator = Evaluator(expression)
+        result = evaluator.evaluate(signal)
+        self.assertEqual(len(result), 1)
+        self.assertDictEqual(result[0], {'entry': 1})
+
+        # assert reference to a module function
+        expression = "{{[{'entry': math.fabs(i) if 1 else None} for i in [1]]}}"
+        evaluator = Evaluator(expression)
+        result = evaluator.evaluate(signal)
+        self.assertEqual(len(result), 1)
+        self.assertDictEqual(result[0], {'entry': 1})
+
+        # assert reference to a builtin function
+        expression = "{{[{'entry': abs(i) if 1 else None} for i in [1]]}}"
+        evaluator = Evaluator(expression)
+        result = evaluator.evaluate(signal)
+        self.assertEqual(len(result), 1)
+        self.assertDictEqual(result[0], {'entry': 1})
+
+        # assert hasattr
+        expression = "{{ hasattr($, 'cond') }}"
+        evaluator = Evaluator(expression)
+        result = evaluator.evaluate(signal)
+        self.assertEqual(result, True)
+
+        # assert type, dict
+        expression = "{{ type($cond) == type(dict()) }}"
+        evaluator = Evaluator(expression)
+        result = evaluator.evaluate(signal)
+        self.assertEqual(result, False)
+
+        # bool conversion grabbing from a dict within signal
+        signal = Signal({'value': {'pre_weight': 0}})
+        expression = "{{ bool($value.get('pre_weight')) }}"
+        evaluator = Evaluator(expression)
+        result = evaluator.evaluate(signal)
+        self.assertEqual(result, False)
+
+        # an expression with a double 'for' statement
+        signal = Signal({'packet': [0x1F, 0x2F, 0x3F, 0x4F, 0x5F,
+                                    0x6F, 0x7F, 0x8F, 0x9F, 0xAF,
+                                    0xBF, 0xCF, 0xDF, 0xFF]})
+        expression = "{{ $packet[10] == 0xFF - (sum([n & 0x0F " \
+                     "for n in $packet[1:10]]) + sum([n >> 4 " \
+                     "for n in $packet[1:10]])) }}"
+        evaluator = Evaluator(expression)
+        result = evaluator.evaluate(signal)
+        self.assertEqual(result, False)
